680c6bf53b2bbfad8d6f4ebeb9ed01c68fa26e63
kalana090 <kalanaperera090@gmail.com>
2023-12-04 11:09:28 +0530

    feature two underline feature added

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function bold() {
     console.log('bold')
     document.execCommand('bold', false, null);
   }
+
+  function underline() {
+    console.log('bold')
+    document.execCommand('underline', false, null);
+  }  
+
+
